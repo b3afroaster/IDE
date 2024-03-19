734abfe40751dd07226ee1b383979f32a84d722c
Dima <arcus.vertebra@mail.ru>
2024-03-20 00:24:55 +0300

    first changes

---
+++ game.py
@@ -0,0 +1,7 @@
+import numpy as np
+number = np.random.randict(1, 101) #make a number
+count = 0
+while True:
+    count += 1
+    predict_number = int(inpput("Do you know the number")
+                         )
---
+++ game_v2.py
@@ -0,0 +1,10 @@
+import numpy as np
+def random_pridict(number: int=1) -> int:
+    """bombastic function that guess number
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Number of tries
+    """
